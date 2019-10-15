2fbf2ae0b7eb4dd2a336f7456e388016765575c3
Shaq <shobbsdaley@gmail.com>
2019-10-15 15:19:10 +0100

    legendary octo and legendaryOcto functions implemented

---
+++ index.js
@@ -0,0 +1,11 @@
+function legendary() {
+  console.log("You heard about that shaq guy? He's legendary!");
+}
+
+function octo() {
+  return 8;
+}
+
+function legendaryOcto() {
+  return "You heard about that shaq guy? He's legendary!".repeat(octo());
+}
